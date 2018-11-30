f9897cf213b047ff913ffb38dfc9fec6059231ff
hp <namstudionsk@gmail.com>
2018-11-30 15:11:00 +0700

    Add person

---
+++ src/main/java/hello/Person.java
@@ -0,0 +1,36 @@
+package hello;
+
+public class Person {
+
+    private String firstName;
+    private String lastName;
+
+    public Person(){
+    }
+
+    public Person(String firstName, String lastName){
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public void setFirstName(String firstName){
+        this.firstName = firstName;
+    }
+
+    public String getFirstName(){
+        return firstName;
+    }
+
+    public void setLastName(String lastName){
+        this.lastName = lastName;
+    }
+
+    public String getLastName(){
+        return lastName;
+    }
+
+    @Override
+    public String toString(){
+        return String.format("firstName: %s, lastName %s", firstName, lastName);
+    }
+}
